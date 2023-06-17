7ffe31ed7f205aa77f979c044cbcba292774dd23
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 02:46:50 +0300

    Add different sectionContent

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,27 +1,58 @@
 import PropTypes from 'prop-types';
+
 import {
   Container,
+  SectionWrapper,
   MainTitle,
   SectionTitle,
-  SectionWrapper,
+  Header,
+  Footer,
+  Main,
 } from './Section.styled';
 
-export const Section = ({ mainTitle, title, children }) => {
-  return (
-    <SectionWrapper>
-      <Container>
-        {mainTitle ? (
-          <MainTitle>{mainTitle}</MainTitle>
-        ) : (
-          <SectionTitle>{title}</SectionTitle>
-        )}
-        {children}
-      </Container>
-    </SectionWrapper>
-  );
+export const Section = ({ tag, mainTitle, title, children }) => {
+  let sectionContent;
+
+  switch (tag) {
+    case 'header':
+      sectionContent = (
+        <Header>
+          <Container>{children}</Container>
+        </Header>
+      );
+      break;
+    case 'main':
+      sectionContent = <Main>{children}</Main>;
+      break;
+    case 'footer':
+      sectionContent = (
+        <Footer>
+          <Container>{children}</Container>
+        </Footer>
+      );
+      break;
+    default:
+      sectionContent = (
+        <SectionWrapper>
+          <Container>
+            {mainTitle ? (
+              <MainTitle>{mainTitle}</MainTitle>
+            ) : (
+              <SectionTitle>{title}</SectionTitle>
+            )}
+            {children}
+          </Container>
+        </SectionWrapper>
+      );
+      break;
+  }
+
+  return sectionContent;
 };
 
 Section.propTypes = {
+  tag: PropTypes.string,
+  mainTitle: PropTypes.string,
   title: PropTypes.string,
   children: PropTypes.any.isRequired,
 };
