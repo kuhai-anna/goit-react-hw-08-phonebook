3d38ddfca0d91f58349aba88102bcb970a39b4f9
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-17 23:38:20 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import { useAuth } from 'hooks';
 import { NavLink } from 'react-router-dom';
 
 export const Navigation = () => {
-  const isLoggedIn = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <nav>
