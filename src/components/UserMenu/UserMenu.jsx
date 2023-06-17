a0c7cf572ad5251a0a48b66ea125b90d5a9585c4
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-17 23:38:48 +0300

    Create UserMenu.jsx

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,17 @@
+import { useAuth } from 'hooks';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/operations';
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <p>{user.email}</p>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
+    </div>
+  );
+};
