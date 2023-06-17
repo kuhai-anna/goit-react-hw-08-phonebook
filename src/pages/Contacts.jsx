379d1faab4fb25ae00e059d3aa68ed08ceb7a29d
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 02:51:14 +0300

    Create Contacts.jsx

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,36 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { Loader } from 'components/Loader/Loader';
+import { Section } from 'components/Section/Section';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; //useSelector
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+
+// import { useLocalStorage } from 'hooks/useLocalStorage';
+
+const Contacts = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoading && !error && <Loader />}
+      <Section mainTitle="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
+  );
+};
+
+export default Contacts;
