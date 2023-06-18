27f6343df20060af4ae18e7976f8d847d7f940a5
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 23:50:23 +0300

    Add welcome text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,60 @@
+import { Button } from '@mui/material';
 import { Section } from 'components/Section/Section';
+import { useAuth } from 'hooks';
+import { Link } from 'react-router-dom';
+
+const styles = {
+  wrapper: {
+    margin: '0 auto',
+    maxWidth: 440,
+    textAlign: 'center',
+  },
+  text: {
+    marginBottom: 8,
+  },
+};
 
 const Home = () => {
-  return <Section mainTitle="Contacts manager welcome page"></Section>;
+  const { isLoggedIn } = useAuth();
+
+  return isLoggedIn ? (
+    <Section mainTitle="Welcome to Phonebook app"></Section>
+  ) : (
+    <Section mainTitle="Phonebook app">
+      <div style={styles.wrapper}>
+        <p style={styles.text}>This app helps you manage your contacts</p>
+
+        <div>
+          <p>
+            To create your account please press{' '}
+            <Button
+              component={Link}
+              to="/register"
+              variant="text"
+              color="primary"
+              size="small"
+            >
+              Register
+            </Button>
+          </p>
+        </div>
+        <div>
+          <p>
+            If you already have an account press{' '}
+            <Button
+              component={Link}
+              to="/login"
+              variant="text"
+              color="primary"
+              size="small"
+            >
+              Log In
+            </Button>
+          </p>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Home;
