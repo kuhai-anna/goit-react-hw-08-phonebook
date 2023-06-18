0da1c1ae604bb79b30ace8db8f48eee8661cbfe3
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 22:51:51 +0300

    Update theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,14 +1,15 @@
 export const theme = {
   colors: {
     text: '#212121',
-    textAccent: '#000000',
+    textAccent: '#0065d1',
     textError: '#ff6b6b',
     bgd: '#f4f4f4',
     bgdAccent: '#d2d2d2',
     border: '#d2d2d2',
-    borderHover: '#a8a8a8',
+    borderHover: '#0065d1',
     btn: '#a8a8a8',
-    btnHover: '#d2d2d2',
+    btnHover: '#f4f4f4',
+    linkBgdHover: '#c9c9c9',
   },
   spacing: value => `${4 * value}px`,
 };
