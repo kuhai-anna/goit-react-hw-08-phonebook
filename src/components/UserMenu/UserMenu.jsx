f1fa839eafda22b70bcfaa85a730f0f7b047f236
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 22:51:31 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,34 @@
-import { useAuth } from 'hooks';
+import { IconButton } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import { useDispatch } from 'react-redux';
+import { useAuth } from 'hooks';
 import { logOut } from 'redux/auth/operations';
+import { UserEmail, Wrapper } from './UserMenu.styled';
+import { theme } from 'constants/theme';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <div>
-      <p>{user.email}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
-        Logout
-      </button>
-    </div>
+    <Wrapper>
+      <UserEmail>
+        <PermIdentityIcon aria-label="Accaunt" />
+        {user.email}
+      </UserEmail>
+      <IconButton
+        aria-label="logout"
+        sx={{
+          color: `${theme.colors.text}`,
+          ':hover': {
+            color: `${theme.colors.textAccent}`,
+          },
+        }}
+        onClick={() => dispatch(logOut())}
+      >
+        <LogoutIcon />
+      </IconButton>
+    </Wrapper>
   );
 };
