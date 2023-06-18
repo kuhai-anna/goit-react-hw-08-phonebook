e234762d4d64a202d5bf68462ab3affffa1041d0
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 22:44:29 +0300

    Add styles to the ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,19 @@
 import { useSelector } from 'react-redux';
 import { ContactListItem } from './ContactListItem/ContactListItem';
 import { selectVisibleContacts } from 'redux/contacts/selectors';
+import { theme } from 'constants/theme';
+
+const styles = {
+  list: {
+    marginTop: `${theme.spacing(4)}`,
+  },
+};
 
 export const ContactList = () => {
   const contacts = useSelector(selectVisibleContacts);
 
   return (
-    <ul>
+    <ul style={styles.list}>
       {contacts?.map(({ id, name, number }) => (
         <ContactListItem key={id} id={id} name={name} number={number} />
       ))}
