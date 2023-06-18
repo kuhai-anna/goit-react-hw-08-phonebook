a529d1a7c625a29c2409be94237d3ee1e6afac4c
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 22:50:57 +0300

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
+export const Header = styled.header`
+  padding: ${props => props.theme.spacing(3)} ${props => props.theme.spacing(0)};
+
+  background-color: ${props => props.theme.colors.bgdAccent};
+`;
+
+export const Main = styled.main`
+  flex: 1;
+`;
+
+export const Footer = styled.footer`
+  padding: ${props => props.theme.spacing(5)} ${props => props.theme.spacing(0)};
+  flex-shrink: 0;
+
+  text-align: center;
+  font-weight: 500;
+  color: ${props => props.theme.colors.text};
+
+  background-color: ${props => props.theme.colors.bgdAccent};
+`;
+
 export const SectionWrapper = styled.section`
   padding: ${props => props.theme.spacing(6)} ${props => props.theme.spacing(0)};
 `;
@@ -7,21 +28,18 @@ export const SectionWrapper = styled.section`
 export const Container = styled.div`
   margin: 0 auto;
   padding: ${props => props.theme.spacing(0)} ${props => props.theme.spacing(4)};
-  width: ${props => props.theme.spacing(80)};
 `;
 
 export const MainTitle = styled.h1`
-  margin-bottom: ${props => props.theme.spacing(5)};
-
-  font-size: 30px;
+  margin-bottom: ${props => props.theme.spacing(6)};
+  font-size: 34px;
   text-align: center;
   color: ${props => props.theme.colors.text};
 `;
 
 export const SectionTitle = styled.h2`
-  margin-bottom: ${props => props.theme.spacing(4)};
-
-  font-size: 24px;
+  margin-bottom: ${props => props.theme.spacing(6)};
   text-align: center;
+  font-size: 30px;
   color: ${props => props.theme.colors.text};
 `;
