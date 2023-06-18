6da95ee487ed95ecddf1da9f9263b35ff715aeba
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 03:01:36 +0300

    Add PersistGate and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
 import 'modern-normalize';
 import { App } from 'components/App';
 import { theme } from './constants/theme';
-import { store } from './redux/store'; // persistor,
+import { store, persistor } from './redux/store';
 import { GlobalStyle } from './GlobalStyle.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
-        <GlobalStyle />
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+            <GlobalStyle />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
