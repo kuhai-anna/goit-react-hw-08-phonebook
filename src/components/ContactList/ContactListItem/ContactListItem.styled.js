75c3bd50864b25c98b1752ee50265fc8ffab4c82
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-18 23:48:59 +0300

    Update ContactListItem.styled.js

--- src/components/ContactList/ContactListItem/ContactListItem.styled.js
+++ src/components/ContactList/ContactListItem/ContactListItem.styled.js
@@ -4,6 +4,10 @@ export const ContactItem = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: ${props => props.theme.spacing(1)} ${props => props.theme.spacing(3)};
+
+  border: 1px solid ${props => props.theme.colors.border};
+  border-radius: ${props => props.theme.spacing(1)};
 
   :not(:last-of-type) {
     margin-bottom: ${props => props.theme.spacing(2)};
@@ -11,42 +15,13 @@ export const ContactItem = styled.li`
 `;
 
 export const Contact = styled.p`
-  display: block;
+  display: flex;
+  align-items: center;
+  gap: ${props => props.theme.spacing(2)};
 
   color: ${props => props.theme.colors.text};
 `;
 
 export const ContactName = styled.span`
-  display: block;
-  margin-bottom: 4px;
-
   font-weight: 500;
-`;
-
-export const ContactNumber = styled.span`
-  display: block;
-`;
-
-export const DeleteButton = styled.button`
-  padding: ${props => props.theme.spacing(1)} ${props => props.theme.spacing(3)};
-
-  outline: none;
-  border: 1px solid transparent;
-  border-radius: ${props => props.theme.spacing(1)};
-  background-color: ${props => {
-    return props.theme.colors.btn;
-  }};
-
-  font-size: 12px;
-  font-weight: 500;
-
-  transition-property: background-color, border-color;
-  transition-duration: 250ms;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-
-  :hover,
-  :focus {
-    background-color: ${props => props.theme.colors.btnHover};
-    border-color: ${props => props.theme.colors.borderHover};
-  }
 `;
